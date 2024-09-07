bd2970dce5011e41ddcfab039d3896384d58a704
Elias0127 <eliasmekonen001@gmail.com>
2024-09-07 16:51:34 -0700

    Add users app and setup authentication

--- coursehub/settings.py
+++ coursehub/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.forum'
+    'apps.forum',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
--- coursehub/urls.py
+++ coursehub/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
+    path('users/', include('users.urls'))
 ]
