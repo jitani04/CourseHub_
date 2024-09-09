06584c798f20006c8e153aa9cd246e0c82140881
Elias0127 <eliasmekonen001@gmail.com>
2024-09-08 17:33:40 -0700

    Add jwt authentication endpoints

--- coursehub-client/src/utils/auth.js
+++ coursehub-client/src/utils/auth.js
@@ -1,10 +1,15 @@
-// Functions for authentication
+import supabase from './supabaseClient';
+
 export const signUp = async (email, password) => {
   return await supabase.auth.signUp({ email, password });
 }
 
 export const signIn = async (email, password) => {
-  return await supabase.auth.signIn({ email, password });
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+  if (data.session) {
+    localStorage.setItem('supabaseToken', data.session.access_token);
+  }
+  return { session: data.session, error };
 }
 
 export const signOut = async () => {
