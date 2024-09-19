1aad35c9508ed427319cd6a51bb5ea6ddbbeda60
Elias0127 <eliasmekonen001@gmail.com>
2024-09-19 15:01:36 -0700

    Restart

--- coursehub/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('forum/', include('forum.urls')),
-    path('users/', include('users.urls'))
-]
