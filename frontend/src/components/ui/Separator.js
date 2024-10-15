658eb6055b7481a0e42f85664ad61d43c78b9b15
Elias0127 <eliasmekonen001@gmail.com>
2024-10-15 16:18:18 -0700

    Add reusable header component with responsivelayout

---
+++ frontend/src/components/ui/Separator.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import { cn } from "../../lib/utils";
+
+const Separator = React.forwardRef(
+  ({ className, orientation = "horizontal", ...props }, ref) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      className={cn(
+        orientation === "horizontal" ? "h-[1px] w-full" : "w-[1px] h-full",
+        "bg-border",
+        className
+      )}
+      {...props}
+    />
+  )
+);
+Separator.displayName = SeparatorPrimitive.Root.displayName;
+
+export { Separator };
